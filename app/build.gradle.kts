148cb62f36d5b291c692d7cf2e661c8795f77513
widlily-corp <widliy-corp@gmail.com>
2026-07-10 17:30:38 +0500

    fix(build): change package ID to com.widlily.wicompress.app to clear launcher icon cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.widlily.wicompress"
+        applicationId = "com.widlily.wicompress.app"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
