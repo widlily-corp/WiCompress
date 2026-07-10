c280a870d1efb89016d42665dedea88efaa35e0c
widlily-corp <widliy-corp@gmail.com>
2026-07-10 17:08:32 +0500

    fix(ui): add missing dp unit import in MainActivity.kt

--- app/src/main/java/com/widlily/wicompress/MainActivity.kt
+++ app/src/main/java/com/widlily/wicompress/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
